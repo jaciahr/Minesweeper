a5d375afb47aa69b38e007643bd436c1dcd61008
jacia <jacia@DESKTOP-LDN9JIH>
2021-08-01 13:06:01 -0400

    final

--- Project3/Project3.cpp
+++ Project3/Project3.cpp
@@ -247,7 +247,7 @@ int main()
                         if (!gameStatus) {
                             debugClicked = false;
                         }
-                        else if (board.GameWin(mineCount, blocksWidth, blocksHeight)) {
+                        if (board.GameWin(mineCount, blocksWidth, blocksHeight)) {
                             debugClicked = false;
                         }
                         else {
